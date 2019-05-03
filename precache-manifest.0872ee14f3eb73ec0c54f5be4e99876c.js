81aa9f1fa9df7c289af2414bd5b37af0d4b3da5d
Neelam Yangal <neelam.yangal@gmail.com>
2019-05-03 12:25:55 +0100

    Updates

---
+++ precache-manifest.0872ee14f3eb73ec0c54f5be4e99876c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0874846f16cb825dc82819a0d6dc8d8",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "d7fa3c50f57e859ba6cd",
+    "url": "/my-react-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/my-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d7fa3c50f57e859ba6cd",
+    "url": "/my-react-app/static/js/main.b7374792.chunk.js"
+  },
+  {
+    "revision": "b28cb091c153ab1cf392",
+    "url": "/my-react-app/static/js/runtime~main.f5ab8794.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
